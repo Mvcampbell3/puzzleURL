4e5d95b1a53af390f82d3b38b6e4b596de66bf4e
Mvcampbell3 <mvcampbell3@gmail.com>
2020-11-07 10:38:44 -0500

    basic create-react-app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Don't we need to import React into every component</h1>
+    </div>
+  );
+}
+
+export default App;
